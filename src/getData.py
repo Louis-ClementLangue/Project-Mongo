e681faee7a637616e89c46e94500c669603d69bb
LC <lc>
2020-10-05 13:41:16 +0200

    clean

--- src/getData.py
+++ src/getData.py
@@ -6,7 +6,6 @@ def get_vlille():
     url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
     response = requests.request("GET", url)
     response_json = json.loads(response.text.encode('utf8'))
-    #select  record part of  object
     return response_json.get("records", [])
 
 
