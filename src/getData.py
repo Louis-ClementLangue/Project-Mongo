1cdfd86d0b0b5f296cb0a4a6575bd945f8945a0b
LC <lc>
2020-10-12 09:59:51 +0200

    test

--- src/getData.py
+++ src/getData.py
@@ -3,6 +3,7 @@ import requests
 import json
 
 # Question 1
+from src import dbi
 
 
 def get_vlille():
@@ -39,15 +40,7 @@ def update_db():
         "mongodb+srv://" + dbi.db_user + ":" + dbi.db_password +
         "@cluster0.yxfmb.gcp.mongodb.net/" + dbi.db_name + "?retryWrites=true&w=majority")
 
-    db = client.vls
-    refresh_stations = p1.get_stations(city)
-
-    for sta in refresh_stations:
-        db.stations.update_one({
-            "name": sta["name"]
-        }, {
-            "$set": sta
-        }, upsert=True)  # Add the data if not found
+    db = client.bikeAvailable
 
 
 if __name__ == '__main__':
@@ -59,3 +52,4 @@ if __name__ == '__main__':
     print(vparis_data)
     print(vlyon_data)
     print(vrennes_data)
+    update_db()
