4c1c4658eddef2dc525a8e43b5f0054e22ce7dc9
LC <lc>
2020-10-12 08:56:39 +0200

    vparis request

--- src/getData.py
+++ src/getData.py
@@ -9,6 +9,15 @@ def get_vlille():
     return response_json.get("records", [])
 
 
+def get_vparis():
+    url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    return response_json.get("records", [])
+
+
 if __name__ == '__main__':
     vlille_data = get_vlille()
+    vparis_data = get_vparis()
     print(vlille_data)
+    print(vparis_data)
